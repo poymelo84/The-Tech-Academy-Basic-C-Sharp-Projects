c869aa7ac9be8fbd39d4c6d59b92f204bcadf9cf
poymelo84 <jaime.melo@ymail.com>
2026-01-25 11:02:03 -0500

    Branching Assignment

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");  // Program start point
+            Console.WriteLine("Please enter package weight");
+            int weight = Convert.ToInt32(Console.ReadLine());   // Enter weight
+            string result = weight > 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "Please enter the package width";   // Ternary operator. If entered number is greater than 50, condition 1 is ran. Otherwise, it is condition 2.
+            Console.WriteLine(result);
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length");
+            int length = Convert.ToInt32(Console.ReadLine());
+            string result1 = width + height + length > 50 ? "Package too big to be shipped via Package Express." : ""; // Ternary operator to check if total dimension is greater than 50.
+            Console.WriteLine(result1);
+            int total = (length * width * height) * weight / 100; // Mathematical operation to multiply package dimensions divided by 100 to get estimaterd total.
+            Console.WriteLine("Your estimated total for shipping this package is: $" + total + ".00");
+
+        }
+        
+    }
+}
---
+++ BranchingAssignment/BranchingAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BranchingAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BranchingAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("516e6e91-ce3b-47a5-8c2f-8f44f7f94f79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
