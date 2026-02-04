47c203b1d732c139ab7e9d46f69bc37ef8547da8
poymelo84 <jaime.melo@ymail.com>
2026-02-04 12:12:01 -0500

    String Assignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "This is string concatenantion";
+            //string threeStrings = "This strings is.. \"strung\". \nThis string in on a new line. \n \t This string jumped over the fence.";
+            //string filename = @"C:\Users\Jaime";
+
+            //bool trueOrFalse = name.Contains("J");
+            //bool trueOrFalse2 = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("This is to write a paragraph");
+            sb.AppendLine(" on multiple lines");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
