2f088893befe08081e9db3adeddb608fe6a89df4
poymelo84 <jaime.melo@ymail.com>
2026-02-18 11:52:06 -0500

    Main Method Assignment 2

---
+++ MainMethodAssignment2/MainMethodAssignment2/MathOperation1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MainMethodAssignment2
+{
+    class MathOperation1
+    {
+        //Create a method that takes two integers as parameters
+        //Make one parameter optional by providing a deafult value
+        public int MathOperation(int i, int j = 1)
+        {
+            //Perform multiplication on integers and return the result
+            int result = i * j;
+            return result;
+        }
+    }
+}
