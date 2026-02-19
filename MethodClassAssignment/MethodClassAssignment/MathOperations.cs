956aa4b3f86413db6ccb8cab72b86540dbefbd0d
poymelo84 <jaime.melo@ymail.com>
2026-02-19 11:21:37 -0500

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class MathOperations
+    {
+        // Create a void method that takes two integers
+        public void DoMath(int firstNumber, int secondNumber)
+        {
+            // Perform a math operation on the first integer
+            int result = (firstNumber + 20);
+
+            // Display the result of the math operation
+            Console.WriteLine("20 added to the first number is: " + result);
+
+            // Display the second integer
+            Console.WriteLine("The second number is: " + secondNumber);
+            Console.WriteLine(); // Add a blank line for better readability
+
+        }
+
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOperations ops = new MathOperations();
+            Console.WriteLine("Calling in the method by passing the two numbers normally.");
+
+            // Call the method by passing two numbers normally
+            ops.DoMath(10, 25);
+            Console.WriteLine("Calling in the method by passing the two numbers using named parameters.");
+            // Call the method using named parameters
+            Console.WriteLine("firstNumber = 10, secondNumber = 25");
+
+            Console.ReadLine();
+
+        }
+    }
+}
