8b7b92a17fd76ba8e40e057643cf15f3c7c42a40
poymelo84 <jaime.melo@ymail.com>
2026-03-01 08:02:57 -0500

    Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload ==
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            // Handle null cases
+            if (ReferenceEquals(e1, e2))
+                return true;
+
+            if (ReferenceEquals(e1, null) || ReferenceEquals(e2, null))
+                return false;
+
+            return e1.Id == e2.Id;
+        }
+
+        // Overload !=
+        public static bool operator !=(Employee e1, Employee e2)
+        {
+            return !(e1 == e2);
+        }
+
+        //// Override Equals (recommended when overloading ==)
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj is Employee other)
+        //        return this.Id == other.Id;
+
+        //    return false;
+        //}
+
+        //// Override GetHashCode (recommended when overriding Equals)
+        //public override int GetHashCode()
+        //{
+        //    return Id.GetHashCode();
+        //}
+
+
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create an Employee object
+            Employee emp1 = new Employee
+            {
+                Id = 23,
+                FirstName = "Michael",
+                LastName = "Jordan"
+            };
+
+            // Create another Employee object with the same Id but different names
+            Employee emp2 = new Employee
+            {
+                Id = 45,
+                FirstName = "Michael",
+                LastName = "Jordan"
+            };
+
+            // Display the Employee objects
+            Console.WriteLine("The Employee objects are being compared here:");
+            Console.WriteLine();
+            // Display the properties of emp1
+            Console.WriteLine($"emp1 == emp2 --> {emp1 == emp2}");
+            // Display the properties of emp2
+            Console.WriteLine($"emp1 != emp2 --> {emp1 != emp2}");
+
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
