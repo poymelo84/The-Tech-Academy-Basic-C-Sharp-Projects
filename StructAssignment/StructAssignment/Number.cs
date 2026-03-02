737419f40519056fde3c31b7392daf6170e506ce
poymelo84 <jaime.melo@ymail.com>
2026-03-02 10:40:40 -0500

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    struct Number
+    {
+        // Define a public property named Amount of type decimal
+        public decimal Amount { get; set; }
+
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Number struct and assign a value to the Amount property
+            Number myNumber = new Number();
+            myNumber.Amount = 67;
+
+            // Display the value of the Amount property to the console
+            Console.WriteLine(myNumber.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
